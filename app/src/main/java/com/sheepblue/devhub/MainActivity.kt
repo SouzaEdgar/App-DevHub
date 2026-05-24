7a09cabe596f259e43107d7b8cf19a907951e4c4
SouzaEdgar <edgarss2013@gmail.com>
2026-05-24 02:44:14 -0300

    ajustando profile layout

--- app/src/main/java/com/sheepblue/devhub/MainActivity.kt
+++ app/src/main/java/com/sheepblue/devhub/MainActivity.kt
@@ -5,19 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sheepblue.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,26 +41,40 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize(),
                         color = MaterialTheme.colorScheme.background
                     ) {
-                        // TODO: Foto de perfil, Nome, Usuario GitHub, Bio. Collumn -> Row
+                        Row {
+                            Column(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Box {
+                                    Box(modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(120.dp)
+                                        .background(Color.Blue)
+                                    ) { }
+                                    Image(
+                                        modifier = Modifier
+                                            .clip(CircleShape)
+                                            .size(120.dp),
+                                        painter = painterResource(id = R.drawable.ic_user_default),
+                                        contentDescription = null
+                                    )
+                                }
+                                Text(
+                                    "Apelido do usuario",
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 30.sp
+                                )
+                                Text(
+                                    "Nome do usuario",
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text("Descrição do usuario (BIO)")
+                            }
+                        }
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DevHubTheme {
-        Greeting("Android")
     }
 }
