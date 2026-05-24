7ec7f49f3b56215423d538ccdc9f107e2143d234
SouzaEdgar <edgarss2013@gmail.com>
2026-05-23 21:33:05 -0300

    Initial commit

---
+++ app/src/main/java/com/sheepblue/devhub/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.sheepblue.devhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.sheepblue.devhub.ui.theme.DevHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DevHubTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface(modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        // TODO: Foto de perfil, Nome, Usuario GitHub, Bio. Collumn -> Row
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DevHubTheme {
+        Greeting("Android")
+    }
+}
