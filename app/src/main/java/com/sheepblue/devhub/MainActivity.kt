9994ed0fddc425a2fc718d5429c1330144ab2330
SouzaEdgar <edgarss2013@gmail.com>
2026-05-24 23:50:16 -0300

    Dia 3 do desafio

--- app/src/main/java/com/sheepblue/devhub/MainActivity.kt
+++ app/src/main/java/com/sheepblue/devhub/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -32,6 +31,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.sheepblue.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,7 +53,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@Preview(showBackground = true)
+
 @Composable
 fun UserScreen() {
     Row {
@@ -65,24 +65,65 @@ fun UserScreen() {
                 .fillMaxWidth()
                 .height(120.dp)
                 .background(
-                    Color.Blue,
+                    Color.DarkGray,
                     shape = RoundedCornerShape(bottomStart = 22.dp, bottomEnd = 22.dp)
                 )
             ) {
-                Image(
+                AsyncImage(
+                    model = "https://avatars.githubusercontent.com/u/65196924?v=4",
+                    placeholder = painterResource(R.drawable.ic_sharp_account_circle_24),
+                    contentDescription = "User Avatar",
                     modifier = Modifier
                         .size(120.dp)
                         .align(Alignment.BottomCenter)
                         .offset(y = 120.dp/2)
-                        .clip(CircleShape),
-                    painter = painterResource(id = R.drawable.ic_user_default),
-                    contentDescription = null
+                        .clip(CircleShape)
                 )
             }
             Spacer(modifier = Modifier.height(120.dp/2))
             Text(
                 "Apelido do usuario",
-                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp
+            )
+            Text(
+                "Nome do usuario",
+                fontWeight = FontWeight.Bold
+            )
+            Text("Descrição do usuario (BIO)")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserScreenPreview() {
+    Row {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .background(
+                    Color.DarkGray,
+                    shape = RoundedCornerShape(bottomStart = 22.dp, bottomEnd = 22.dp)
+                )
+            ) {
+                AsyncImage(
+                    model = "https://avatars.githubusercontent.com/u/1024025?v=4",
+                    placeholder = painterResource(R.drawable.ic_sharp_account_circle_24),
+                    contentDescription = "User Avatar",
+                    modifier = Modifier
+                        .size(120.dp)
+                        .align(Alignment.BottomCenter)
+                        .offset(y = 120.dp/2)
+                        .clip(CircleShape)
+                )
+            }
+            Spacer(modifier = Modifier.height(120.dp/2))
+            Text(
+                "Apelido do usuario",
                 fontSize = 30.sp
             )
             Text(
