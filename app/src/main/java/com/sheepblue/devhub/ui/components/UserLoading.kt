f4de72b4fb4293c7fd99fd617471dffaf1a8d0b5
SouzaEdgar <edgarss2013@gmail.com>
2026-05-31 18:14:15 -0300

    feat: prepara estrutura do skeleton loading

--- app/src/main/java/com/sheepblue/devhub/ui/components/UserLoading.kt
+++ app/src/main/java/com/sheepblue/devhub/ui/components/UserLoading.kt
@@ -1,38 +1,39 @@
 package com.sheepblue.devhub.ui.components
 
-import androidx.compose.foundation.Image
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.sheepblue.devhub.R
+import com.sheepblue.devhub.ui.theme.DevHubTheme
 
 @Composable
 fun UserLoading() {
     Column(
+        Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
+        Box( // Background
             modifier = Modifier
                 .fillMaxWidth()
                 .height(160.dp)
@@ -44,100 +45,88 @@ fun UserLoading() {
                     )
                 )
         ) {
-            Image(
-                painter = painterResource(
-                    R.drawable.ic_sharp_account_circle_24
-                ),
-                contentDescription = "User Avatar",
-                modifier = Modifier
+            Box(modifier = Modifier
                     .size(160.dp)
                     .align(Alignment.BottomCenter)
-                    .offset(y = 120.dp / 2)
+                    .offset(y = 120.dp/2)
+                    .background(Color.LightGray, shape = CircleShape)
                     .clip(CircleShape)
-            )
+
+            ) // Avatar
         }
-        Spacer(modifier = Modifier.height(160.dp / 2))
-        Text(
-            text = "---",
-            fontSize = 30.sp
-        )
-        Text(
-            text = "--",
-            fontWeight = FontWeight.Bold
-        )
-        Text(text = "-")
-        Spacer(modifier = Modifier.height(40.dp / 2))
-        Card(
-            modifier = Modifier.padding(8.dp),
-            elevation = CardDefaults.cardElevation(4.dp)
-        ) {
-            Column {
-                Text(
-                    text = "----",
-                    modifier = Modifier
+        Spacer(modifier = Modifier.height(180.dp / 2))
+        Box(modifier = Modifier
+                .height(50.dp)
+                .width(250.dp)
+                .padding(bottom = 20.dp)
+                .background(Color.Red, shape = RoundedCornerShape(8.dp))
+                .clip(shape = RoundedCornerShape(8.dp))
+        ) // Nome
+        Box(modifier = Modifier
+            .height(30.dp)
+            .width(170.dp)
+            .background(Color.Blue, shape = RoundedCornerShape(8.dp))
+            .clip(shape = RoundedCornerShape(8.dp))
+        ) // Login
+        Box(modifier = Modifier
+            .height(60.dp)
+            .width(250.dp)
+            .padding(top = 20.dp)
+            .background(Color.Green, shape = RoundedCornerShape(8.dp))
+            .clip(shape = RoundedCornerShape(8.dp))
+        ) // Bio
+        Spacer(modifier = Modifier.height(70.dp / 2))
+
+        // TODO: Ao inves de 3 cards montados manualmente, passar apenas 1 no repeat
+        repeat(3) {
+            Card(
+                modifier = Modifier.padding(16.dp),
+                elevation = CardDefaults.cardElevation(4.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+            ) {
+                Column {
+                    Box(modifier = Modifier
+                        .height(40.dp)
                         .fillMaxWidth()
-                        .background(Color(0xFF2d333b))
-                        .padding(8.dp),
-                    fontSize = 24.sp,
-                    color = Color.White
-                )
-                Text(
-                    text = "----",
-                    modifier = Modifier
-                        .padding(8.dp)
+                        .background(Color.Magenta, shape = RoundedCornerShape(8.dp))
+                        .clip(shape = RoundedCornerShape(8.dp))
+                    ) // Titulo da repo
+                    Box(modifier = Modifier
+                        .height(50.dp)
                         .fillMaxWidth()
-                )
-            }
-        }
-        Card(
-            modifier = Modifier.padding(8.dp),
-            elevation = CardDefaults.cardElevation(4.dp)
-        ) {
-            Column {
-                Text(
-                    text = "----",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color(0xFF2d333b))
-                        .padding(8.dp),
-                    fontSize = 24.sp,
-                    color = Color.White
-                )
-                Text(
-                    text = "----",
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth()
-                )
-            }
-        }
-        Card(
-            modifier = Modifier.padding(8.dp),
-            elevation = CardDefaults.cardElevation(4.dp)
-        ) {
-            Column {
-                Text(
-                    text = "----",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color(0xFF2d333b))
-                        .padding(8.dp),
-                    fontSize = 24.sp,
-                    color = Color.White
-                )
-                Text(
-                    text = "----",
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth()
-                )
+                        .background(Color.Yellow, shape = RoundedCornerShape(8.dp))
+                        .clip(shape = RoundedCornerShape(8.dp))) // Descrição da repo
+                }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    name = "Light",
+    showBackground = true
+)
 @Composable
 fun UserLoadingPreview() {
-    UserLoading()
+    DevHubTheme{
+        UserLoading()
+    }
 }
+
+@Preview(
+    name = "Dark",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun UserLoadingDarkPreview() {
+    DevHubTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            UserLoading()
+        }
+    }
+}
+
