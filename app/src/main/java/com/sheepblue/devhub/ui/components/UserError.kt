81c8b542c109586935bc6ef9bb2b0110d814318f
SouzaEdgar <edgarss2013@gmail.com>
2026-06-01 15:05:25 -0300

    style: altera estilo do componente UserError

--- app/src/main/java/com/sheepblue/devhub/ui/components/UserError.kt
+++ app/src/main/java/com/sheepblue/devhub/ui/components/UserError.kt
@@ -11,13 +11,16 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,11 +43,20 @@ fun UserError() {
                     )
                 )
         ) {
+            Box(
+                modifier = Modifier
+                    .size(160.dp)
+                    .align(Alignment.BottomCenter)
+                    .offset(y = 120.dp / 2)
+                    .background(MaterialTheme.colorScheme.background, shape = CircleShape)
+                    .clip(CircleShape)
+            )
             Image(
                 painter = painterResource(
                     R.drawable.ic_sharp_account_circle_24
                 ),
                 contentDescription = "User Avatar",
+                colorFilter = ColorFilter.tint(Color.DarkGray),
                 modifier = Modifier
                     .size(160.dp)
                     .align(Alignment.BottomCenter)
@@ -52,10 +64,11 @@ fun UserError() {
                     .clip(CircleShape)
             )
         }
-        Spacer(modifier = Modifier.height(160.dp / 2))
+        Spacer(modifier = Modifier.height(180.dp / 2))
         Text(
             text = "Não foi possível encontrar usuário",
-            fontSize = 20.sp
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
         )
     }
 }
