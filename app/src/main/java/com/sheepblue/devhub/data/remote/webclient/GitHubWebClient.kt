6e09a4235f29f706f63483729036da0852aba23c
SouzaEdgar <edgarss2013@gmail.com>
2026-06-11 21:28:50 -0300

    refactor: separa a response da API do UI State

--- app/src/main/java/com/sheepblue/devhub/data/remote/webclient/GitHubWebClient.kt
+++ app/src/main/java/com/sheepblue/devhub/data/remote/webclient/GitHubWebClient.kt
@@ -2,38 +2,66 @@ package com.sheepblue.devhub.data.remote.webclient
 
 import android.util.Log
 import com.sheepblue.devhub.data.RetrofitInitializer
+import com.sheepblue.devhub.data.remote.model.GitHubProfileWeb
+import com.sheepblue.devhub.data.remote.model.GitHubRateLimit
+import com.sheepblue.devhub.data.remote.model.GitHubRepository
+import com.sheepblue.devhub.data.remote.model.GitHubResponse
 import com.sheepblue.devhub.data.remote.service.GitHubService
-import com.sheepblue.devhub.ui.state.UserProfileUiState
-import com.sheepblue.devhub.ui.state.UserRepositoryUiState
 
 class GitHubWebClient (private val service: GitHubService = RetrofitInitializer().gitHubService) {
+    private val nullGitHubProfile = GitHubProfileWeb(
+        login = "",
+        name = "",
+        bio = "",
+        avatar_url = ""
+    )
 
-    suspend fun findProfileBy(user: String): UserProfileUiState {
+    suspend fun findProfileBy(user: String): GitHubResponse {
         val profileResponse = service.findProfileBy(user)
-        val profileResponseBody = profileResponse.body()
-        val profileResponseHeader = profileResponse.headers()
+        val profileInfos = profileResponse.body()
+
+        val currentLimit = GitHubRateLimit(
+            limit = profileResponse.headers()["x-ratelimit-limit"]?.toLongOrNull(),
+            remaining = profileResponse.headers()["x-ratelimit-remaining"]?.toLongOrNull(),
+            reset = profileResponse.headers()["x-ratelimit-reset"]?.toLongOrNull(),
+            used = profileResponse.headers()["x-ratelimit-used"]?.toLongOrNull()
+        )
 
         val reposResponse = service.findRepositoryBy(user)
+        val profileRepos: List<GitHubRepository> = reposResponse.map { repo ->
+            GitHubRepository(
+                name = repo.name,
+                description = repo.description
+            )
+        }
 
-        Log.d("DEBUG", "Restantes: ${profileResponseHeader.get("x-ratelimit-remaining")}\n+1 em: ${profileResponseHeader["x-ratelimit-reset"]}\nreposResponse: $reposResponse")
+        Log.d("DEBUG",
+            "Restantes: ${profileResponse.headers()["x-ratelimit-remaining"]}" +
+                    "\n+1 em: ${profileResponse.headers()["x-ratelimit-reset"]}" +
+                    "\nreposResponse: $reposResponse")
 
-        // retorna com a conversao ja feita (mapper)
-        return UserProfileUiState(
-            login = profileResponseBody?.login ?: "",
-            name = profileResponseBody?.name?: "~ sem nome ~",
-            bio = profileResponseBody?.bio?: "~ sem bio ~",
-            image = profileResponseBody?.avatar_url?: "",
-            repositories = reposResponse.map { repo ->
-                UserRepositoryUiState(
-                    name = repo.name?: "",
-                    description = repo.description?: ""
-                )
-            }
-//            login = "login_teste",
-//            name = "name_teste",
-//            bio = "bio_teste",
-//            image = "",
-//            repositories = emptyList()
+        return GitHubResponse(
+            profile = profileInfos?: nullGitHubProfile,
+            repositories = profileRepos,
+            rateLimit = currentLimit
         )
     }
 }
+
+//                return UserProfileUiState(
+//                    login = profileResponseBody?.login ?: "",
+//                    name = profileResponseBody?.name?: "~ sem nome ~",
+//                    bio = profileResponseBody?.bio?: "~ sem bio ~",
+//                    image = profileResponseBody?.avatar_url?: "",
+//                    repositories = reposResponse.map { repo ->
+//                        UserRepositoryUiState(
+//                            name = repo.name?: "",
+//                            description = repo.description?: ""
+//                        )
+//                    }
+////            login = "login_teste",
+////            name = "name_teste",
+////            bio = "bio_teste",
+////            image = "",
+////            repositories = emptyList()
+//                )
--- app/src/main/java/com/sheepblue/devhub/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/sheepblue/devhub/viewmodel/UserViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sheepblue.devhub.data.remote.webclient.GitHubWebClient
 import com.sheepblue.devhub.ui.state.UserProfileUiState
+import com.sheepblue.devhub.ui.state.UserRepositoryUiState
 import kotlinx.coroutines.launch
 
 class UserViewModel(private val repository: GitHubWebClient): ViewModel() {
@@ -23,7 +24,27 @@ class UserViewModel(private val repository: GitHubWebClient): ViewModel() {
             )
 
             try {
-                uiState = repository.findProfileBy(user)
+                //uiState = repository.findProfileBy(user)
+                uiState = repository.findProfileBy(user = user).profile
+            // montar a uistate com os valores de profile
+                // retorna com a conversao ja feita (mapper)
+//                return UserProfileUiState(
+//                    login = profileResponseBody?.login ?: "",
+//                    name = profileResponseBody?.name?: "~ sem nome ~",
+//                    bio = profileResponseBody?.bio?: "~ sem bio ~",
+//                    image = profileResponseBody?.avatar_url?: "",
+//                    repositories = reposResponse.map { repo ->
+//                        UserRepositoryUiState(
+//                            name = repo.name?: "",
+//                            description = repo.description?: ""
+//                        )
+//                    }
+////            login = "login_teste",
+////            name = "name_teste",
+////            bio = "bio_teste",
+////            image = "",
+////            repositories = emptyList()
+//                )
             } catch (e: Exception) {
                 uiState = uiState.copy(
                     isLoading = false,
